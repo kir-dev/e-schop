74f3567310371125dd4143c9685e09ffc0eb4c4c
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2019-08-06 19:55:35 +0200

    remove variants

--- app/models/good.rb
+++ app/models/good.rb
@@ -5,15 +5,5 @@ class Good < ApplicationRecord
   validates_numericality_of :price, presence: true
   has_one_attached :photo
 
-  def thumb
-    self.photo.variant(resize: '300x300').processed
-  end
-
-  def thumb_list
-    self.photo.variant(resize: '320!x320!').processed
-  end
-
-  def thumb_cart
-    self.photo.variant(resize: '100x100!').processed
-  end
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,15 +2,5 @@ class Product < ApplicationRecord
   has_one :category
   has_one_attached :photo
 
-  def thumb
-    self.photo.variant(resize: '300x300').processed
-  end
-
-  def thumb_list
-    self.photo.variant(resize: '320x320!').processed
-  end
-
-  def thumb_cart
-    self.photo.variant(resize: '100x100!').processed
-  end
+  
 end
