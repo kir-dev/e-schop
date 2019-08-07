93924fa2123b206eea01f9efff3c32b06d37af64
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2019-08-07 12:16:15 +0200

    move listelement view logic to helper

--- app/helpers/goods_helper.rb
+++ app/helpers/goods_helper.rb
@@ -1,2 +1,19 @@
 module GoodsHelper
+
+
+    def getImage(good)
+        
+        
+        if good.photo.attached? 
+            image=  good.photo
+        else 
+            product = @products.find_by_id(good.product_id) 
+            if product.photo.attached? 
+                image=product.photo
+            end  
+        end 
+        image
+    end
+
+
 end
