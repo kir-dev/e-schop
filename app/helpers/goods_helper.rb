0834057768b73facdd57323cee9ccf59bf691cfa
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2019-08-14 14:36:01 +0200

    update image selection

--- app/helpers/goods_helper.rb
+++ app/helpers/goods_helper.rb
@@ -1,19 +1,33 @@
 module GoodsHelper
 
     def get_image(good)
+        
+    
         if good.photo.attached? 
-            image=  good.photo
-        else 
-            if defined?@products
+           return image_tag  good.photo
+            puts "good"
+
+        elsif @products.any?{|p| p.id == good.product_id}
                 product = @products.find{|p| p.id == good.product_id}
                 if product.photo.attached? 
-                    image=product.photo
-                end  
-            else
-                image=Product.find_by_id(good.product_id).photo
+                  return  image_tag product.photo
+                    puts "products"
+
+                end
+        else 
+
+            product = Product.find_by_id(good.product_id)
+            if product.photo.attached? 
+              return  image_tag product.photo
+                puts "database"
+                 
+            
+
             end
-        end 
-        image
+        end
+        
+        image_tag 'default.jpg'
+        
     end
 
 end
