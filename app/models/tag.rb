6a79d28afc4945c9edb66397c7f81da62e00dad1
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2019-08-28 21:34:40 +0200

    add lowercase validation to tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,10 @@ class Tag < ApplicationRecord
     has_and_belongs_to_many :goods
     has_many :intrests
     has_many :users, through: :intrests
+    validate :name_must_be_lowercase
+
+    def name_must_be_lowercase
+        self.name=self.name.downcase
+    end
+
 end
