ac2139a1eeb8a06654dbf27465ed386044e55437
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2019-09-03 20:21:07 +0200

    add login test

--- test/system/goods_test.rb
+++ test/system/goods_test.rb
@@ -2,15 +2,22 @@ require "application_system_test_case"
 
 class GoodsTest < ApplicationSystemTestCase
 
-  test "visiting the index" do
+  setup do
     30.times do
       create(:good)
     end
     create(:user)
+  end
 
-    visit (goods_path)
-    expect(page).to have_current_path(goods_pathf)
+
+  test "visiting the index" do
     
-   
+    visit (goods_path)
+    click_on "Bejelentkezés"  
+    fill_in "user_email", with: "user@gmail.com"
+    fill_in "user_password", with: "secret"
+    click_on "Log in"
+    
+    assert_current_path(root_path)
   end
 end
