28f1891344000e65a6fd573dee42a390ebda73d8
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2019-09-06 12:25:03 +0200

    move factories to rspec

--- spec/factories/goods.rb
+++ spec/factories/goods.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :good do
-    name {"Test good"}
+    id { Good.all.size + 1 }
+    name { 'Test good' }
     category_id { 1 }
-    description{ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac sagittis metus. Etiam id felis quis nulla elementum accumsan at fringilla lacus. Proin tempus iaculis feugiat. Proin consequat diam diam, vel dignissim mi semper ut. Nulla facilisi. Sed molestie mauris at ipsum maximus pulvinar. Ut consequat eu lacus at ultrices. Curabitur consequat et dolor vel pretium. Proin ac fringilla nunc, sit amet consectetur dolor. Suspendisse tortor lacus, pellentesque tempus ipsum id, molestie dignissim metus. Donec placerat tellus augue, eget maximus orci placerat pulvinar." }
+    description { 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac sagittis metus. Etiam id felis quis nulla elementum accumsan at fringilla lacus. Proin tempus iaculis feugiat. Proin consequat diam diam, vel dignissim mi semper ut. Nulla facilisi. Sed molestie mauris at ipsum maximus pulvinar. Ut consequat eu lacus at ultrices. Curabitur consequat et dolor vel pretium. Proin ac fringilla nunc, sit amet consectetur dolor. Suspendisse tortor lacus, pellentesque tempus ipsum id, molestie dignissim metus. Donec placerat tellus augue, eget maximus orci placerat pulvinar.' }
     number { 3 }
     seller_id { User.first.id }
     price { 2000 }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    email {"user@gmail.com"}
-    password{'secret'}
-    username{'User'}
-    roomnumber {'707'}
+    email { 'user@gmail.com' }
+    password { 'secret' }
+    username { 'User' }
+    roomnumber { '707' }
   end
 end
