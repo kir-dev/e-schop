76a9048fb8699dfde64d475842b206c9023a81ea
Csaba591 <csaba591@gmail.com>
2019-10-01 17:58:56 +0200

    create subscriptions to all existing conversations

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -2,6 +2,9 @@ class ConversationChannel < ApplicationCable::Channel
   def subscribed
     conversation = Conversation.find(params[:id])
     stream_for conversation
-    # stream_from "conversation_channel_#{params[:id]}"
+  end
+
+  def unsubscribed
+    stop_all_streams
   end
 end
