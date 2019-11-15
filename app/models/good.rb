9b6ef9d7da2f26589b256803d2da8bffd661dc36
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2019-11-15 23:02:24 +0100

    fix XSS

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class Good < ApplicationRecord
+  include ApplicationHelper
+
   acts_as_paranoid
   belongs_to :user, optional: true
   validates :name, presence: true
   validates_numericality_of :price, presence: true
   has_one_attached :photo
   has_and_belongs_to_many :tags
+
+  after_save :set_filename
+
 
   def thumb
     photo.variant(resize: '300x300!').processed
